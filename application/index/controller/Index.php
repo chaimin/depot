5666b9facbfbfe4dcec4d6ff0d4873a127547216
scaooo <1577697795@qq.com>
2019-11-14 09:29:11 +0800

    2

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,11 +6,18 @@ class Index
     //Input $input, Output $output
     public function index()
     {
-        echo 111;
-//                $admin_cate_id = db::name('admin')->where('id',1)->value('admin_cate_id');
+        echo 333;
+//        $admin_cate_id = db::name('admin')->where('id',1)->value('admin_cate_id');
 //        $where = [];
 //        $where['admin_cate_id'] = $admin_cate_id + 1;
-//        db::name('admin')->where('id',1)->update($where);
+//        if(db::name('admin')->where('id',1)->update($where))
+//        {
+//            echo 11;
+//        }
+//        else
+//        {
+//            echo 22;
+//        }
     }
 
     public function foraa()
