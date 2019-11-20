0eefe74248e869d18db793cf890d44aa78d80d61
scaooo <1577697795@qq.com>
2019-11-20 11:51:54 +0800

    user

---
+++ application/index/controller/User.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+class User
+{
+    //Input $input, Output $output
+    public function index()
+    {
+        $admin_cate_id = db::name('admin')->where('id',1)->select();
+        echo  db::name('admin')->getLastSql();
+        die;
+        $admin_cate_id = db::name('admin')->where('id',1)->value('admin_cate_id');
+        $where = [];
+        $where['admin_cate_id'] = $admin_cate_id + 1;
+        if(db::name('admin')->where('id',1)->update($where))
+        {
+            // echo 11;
+        }
+        else
+        {
+            echo 22;
+        }
+    }
+}
