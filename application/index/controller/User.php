949a02b5ab877a53ebd006bc764fc131555d8760
scaooo <1577697795@qq.com>
2019-11-20 12:31:24 +0800

    11

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -6,15 +6,13 @@ class User
     //Input $input, Output $output
     public function index()
     {
-        $admin_cate_id = db::name('admin')->where('id',1)->select();
-        echo  db::name('admin')->getLastSql();
-        die;
+
         $admin_cate_id = db::name('admin')->where('id',1)->value('admin_cate_id');
         $where = [];
         $where['admin_cate_id'] = $admin_cate_id + 1;
         if(db::name('admin')->where('id',1)->update($where))
         {
-            // echo 11;
+             echo 11;
         }
         else
         {
